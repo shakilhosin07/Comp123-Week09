9e92515fe51b23976762c49d856ba058426de313
Zakir <Zakir@LAPTOP-GQEFR3RC>
2017-07-11 15:41:40 -0400

    Added Superhuman subclass

---
+++ Comp123-Week09/Human.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * shakil hosin
+ * july 11th 2017
+ * this is the abstract class human which other 
+ * subclasses will derive from
+ * Version : 0.1 - Class created
+ */
+namespace Comp123_Week09
+{
+    // <summary>
+    /// this is the human abstract class
+    /// </summary>
+    
+    public abstract class Human
+    {
+        //private fields (instance variables)
+        private string _name;
+        // public properties 
+        private string Name {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+
+
+
+        }
+        //Constructors
+        /// <summary>
+        /// This is the constructor for the human abstract class.
+        /// it requires one parameter - name (string)
+        /// </summary>
+        /// <param name="name"></param>
+        public Human(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ Comp123-Week09/SuperHuman.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name: Shakil Hosin
+ * Date: July 11th, 2017
+ * Description: This is the Superhuman sub class.
+ * Version : 0.1 - Created the SuperHuman sub class
+ */ 
+namespace Comp123_Week09
+{
+    /// <summary>
+    /// Thsi is the SuperHuman sub class.
+    /// It inherits fromt he Human Superclass
+    /// </summary>
+    public class SuperHuman : Human
+    {
+        // PRIVATE FIELDS
+        //PUBLIC PROPERTIES
+        //CONSTRUCTORS
+        public SuperHuman(string name)
+            :base(name)
+        {
+
+        }
+        //PUBLIC METHODS
+    }
+}
