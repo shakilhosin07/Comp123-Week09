5b76eb8be419c8e64e724b2c36eec055599032e2
Zakir <Zakir@LAPTOP-GQEFR3RC>
2017-07-11 15:46:03 -0400

    Added a new data type

---
+++ Comp123-Week09/Power.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name: Shakil Hosin
+ * Date: July 11th, 2017
+ * Description: This is the Power data type.
+ * Version : 0.1 - Power class created
+ */
+namespace Comp123_Week09
+{
+    /// <summary>
+    /// The power class contains the data and structure of the each super power.
+    /// </summary>
+    public class Power
+    {
+        public string Name { get; set; }
+        public int Rank { get; set; }
+    }
+}
