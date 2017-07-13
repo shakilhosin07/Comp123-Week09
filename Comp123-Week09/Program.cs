697e00d2006ed493bb0bd08c868f86d38e37a580
Zakir <Zakir@LAPTOP-GQEFR3RC>
2017-07-13 12:13:18 -0400

    Created a new object of the SuperVillain class

--- Comp123-Week09/Program.cs
+++ Comp123-Week09/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
  * shakil hosin
  * july 11th 2017
  * This is the driver class for my project
- * Version : 0.5 - Implemented the Tostring method of the superhuman class
+ * Version : 0.6 - Created a new object of the SuperVillain class
  */
 namespace Comp123_Week09
 {
@@ -15,11 +15,11 @@ namespace Comp123_Week09
     {
         static void Main(string[] args)
         {
-            SuperVillain superVillain = new SuperVillain("Green Goblin", 20);
+            SuperVillain superVillain = new SuperVillain("Green Goblin", 10);
+            Console.WriteLine(superVillain.ToString());
             SuperHuman superHuman = new SuperHuman("Super Dude");
             superHuman.Powers.Add(new Power("Teleportation", 50));
             superHuman.AddPower("Webcasting", 40);
-            superHuman.DisplayPowers();
             Console.WriteLine(superHuman.ToString());
         }
         
