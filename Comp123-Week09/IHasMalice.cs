97bbf761e37c4a42ba3660dd346b232ad166c766
Zakir <Zakir@LAPTOP-GQEFR3RC>
2017-07-13 11:55:42 -0400

    Declared the malice property in the IHasMalice interface

---
+++ Comp123-Week09/IHasMalice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name: Shakil Hosin
+ * Date: July 13th, 2017
+ * Description: This interface lists the signature for the SuperVillain subclass.
+ * Version 0.1 : Declared the malice property 
+ */
+namespace Comp123_Week09
+{
+    public interface IHasMalice
+    {
+        //public property 
+        int Malice { get; set; }
+    }
+}
